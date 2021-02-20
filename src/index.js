37f3caefce9f93f96e7a4daef1769372ad60274a
Miguel Angel Tomairo <matomairo@gmail.com>
2021-02-20 01:18:12 -0500

    refactor swagger

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const swaggerOptions={
             servers:["http://localhost:3000"]
         }
     },
-    apis:["index.js"]
+    apis:["src/index.js"]
 }
 const swaggerDocs=swaggerJSDoc(swaggerOptions);
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
@@ -36,6 +36,78 @@ var corsOptions={
 }
 
 app.use(express.json());
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *      Employee:
+ *       type: object
+ *       properties:
+ *          name:
+ *              type: string
+ *              description: name of the employee
+ *          date_of_joining:
+ *              type: string
+ *              description: date of joining of the employee
+ *          email:
+ *              type: string
+ *              description: email of the employee
+ *          gender:
+ *              type: string
+ *              description: gender of the employee
+ *          bio:
+ *              type: string
+ *              description: biography of the employee
+ *          designation:
+ *              type: string
+ *              description: designation of the employee
+ *       example:
+ *         name: Bruno
+ *         date_of_joining: 20-04-2021
+ *         email: brunitoz@gmail.com
+ *         gender: male
+ *         bio: ingeniero, emprendedor, innovador
+ *         designation: ios developer
+ */
+
+ /**
+ * @swagger
+ * components:
+ *   schemas:
+ *       Team:
+ *          type: object
+ *          properties:
+ *              name:
+ *                  type: string
+ *                  description: name of the team
+ *              email:
+ *                  type: string
+ *                  description: email of the team
+ *              description:
+ *                  type: string
+ *                  description: description of the team
+ *          example:
+ *              name: Apple
+ *              email: steve@gmail.com
+ *              author: Miguel Rapser
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Employee
+  *   description: The employee managing API
+  */
+
+  /**
+  * @swagger
+  * tags:
+  *   name: Team
+  *   description: The team managing API
+  */
+
+
 /**
  * @swagger
  * definitions:
@@ -100,6 +172,7 @@ app.use(express.json());
   * /employee:
   *  post:
   *   summary: create employee
+  *   tags: [Employee]
   *   description: create employee for the organisation
   *   requestBody:
   *    content:
@@ -118,6 +191,7 @@ app.post("/employee", createEmployee);
  * /team:
  *  post:
  *   summary: create team
+ *   tags: [Team]
  *   description: create team
  *   parameters:
  *    - in: body
@@ -143,6 +217,7 @@ app.post("/team", createTeam);
  * /employeeassignment:
  *  post:
  *   summary: create employee assignment
+ *   tags: [EmployeeAssign]
  *   description: create employee assignment
  *   parameters:
  *    - in: body
@@ -169,6 +244,7 @@ app.post("/employeeassignment",createEmployeeAssignment);
  * /employees:
  *  get:
  *   summary: get all employees
+ *   tags: [Employee]
  *   description: get all employees
  *   responses:
  *    200:
@@ -182,6 +258,7 @@ app.get('/employees',cors(), getAllEmployees)
  * /teams:
  *  get:
  *   summary: get all teams
+ *   tags: [Team]
  *   description: get all teams
  *   responses:
  *    200:
@@ -193,6 +270,7 @@ app.get('/teams',cors(), getAllTeams)
  * /employee/{employee_id}:
  *  get:
  *   summary: get employee
+ *   tags: [Employee]
  *   description: get employee
  *   parameters:
  *    - in: path
@@ -212,6 +290,7 @@ app.get('/employee/:id',cors(), getEmployee)
  * /team/{team_id}:
  *  get:
  *   summary: create team
+ *   tags: [Team]
  *   description: create team
  *   parameters:
  *    - in: path
@@ -232,6 +311,7 @@ app.get('/team/:id',cors(), getTeam)
  * /employee/{id}:
  *  put:
  *   summary: update employee
+ *   tags: [Employee]
  *   description: update employee
  *   consumes:
  *    - application/json
@@ -272,6 +352,7 @@ app.put("/employee/:id",cors(corsOptions),updateEmployee)
  * /team/{id}:
  *  put:
  *   summary: update team
+ *   tags: [Team]
  *   description: update team
  *   consumes:
  *    - application/json
@@ -311,6 +392,7 @@ app.put("/team/:id",cors(corsOptions),updateTeam)
  * /employee/{employee_id}:
  *  delete:
  *   summary: delete employee
+ *   tags: [Employee]
  *   description: delete employee
  *   parameters:
  *    - in: path
@@ -332,6 +414,7 @@ app.delete("/employee/:id",deleteEmployee)
  * /team/{team_id}:
  *  delete:
  *   summary: delete team
+ *   tags: [Team]
  *   description: delete team
  *   parameters:
  *    - in: path
@@ -351,6 +434,7 @@ app.delete("/team/:id",deleteTeam)
  * /employeeassign/{employee_id}/{team_id}:
  *  delete:
  *   summary: delete employee assignment
+ *   tags: [EmployeeAssign]
  *   description: delete employee assignment
  *   parameters:
  *    - in: path
